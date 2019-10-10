c9882e2218a3318e7dab020460ed20a2f12025bd
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-10 21:04:31 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,63 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+  # @@paintings = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
-
+  
+  def self.all 
+    @@all   
+  end
+  
+  
+  
+  def paintings
+    #returns an array of all the paintings
+    # old code - @@paintings
+    
+    paintings = Painting.all 
+    paintings.select do |painting|
+      painting.artist == self
+    end
+  end
+  
+  def self.total_experience
+    total = 0
+    
+    all.each do |artist|
+      total += artist.years_experience
+    end
+    
+    return total
+  end
+  
+  def create_painting(title, price, gallery)
+    painting = Painting.new(title, price, gallery, self)
+  end
+  
+  def cities
+    #return an array of all cities that an artist has paintings in 
+    #paintings = Paintings.all
+    # paintings.select do |painting|
+    #   painting.city == 
+    # end
+  end
+  
+  def galleries
+    #Returns an array of all the galleries that an artist has paintings in
+    paintings.collect do |painting|
+      painting.gallery
+    end
+  end
+  
+  def cities 
+    galleries.collect do |gallery|
+      gallery.city
+    end
+  end
 end
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,38 @@
 class Gallery
-
+  @@all = []
   attr_reader :name, :city
 
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
   end
-
+  
+  def self.all 
+    @@all
+  end
+  
+  def paintings
+    paintings = Painting.all 
+    paintings.select do |painting|
+      painting.gallery == self
+    end
+  end
+  
+  def artists
+    #returns an array of all artists that have a painting in a gallery
+    paintings.collect do |painting|
+      painting.artist 
+    end
+  end
+  
+  def artist_names
+    artists.collect do |artist|
+      artist.name 
+    end
+  end
+  
+  def most_expensive_painting
+    paintings.max { |p1, p2| p1.price <=> p2.price }
+  end
 end
