efe9b955e3530fb5554802832e4081a44025e124
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-10 21:16:30 +0000

    Done.

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,39 @@
 class Painting
 
-  attr_reader :title, :price
+  attr_reader :title, :price, :gallery, :artist
 
   @@all = []
 
-  def initialize(title, price)
+  def initialize(title, price, gallery, artist)
     @title = title
     @price = price
+    @gallery = gallery
+    @artist = artist
+    @@all << self
   end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def self.total_price 
+    total = 0
+    
+    all.each do |painting|
+      total += painting.price
+    end
+    
+    return total
+  end
+  
+  def artist=(artist)
+    @artist = artist
+    #artist.save_painting(self)
+  end
+  
+  def gallery=(gallery)
+    @gallery = gallery
+  end
+  
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,5 +1,14 @@
 require_relative '../config/environment.rb'
 
+nick = Artist.new("Nick", 1)
+matt = Artist.new("Matt", 420)
+
+g1 = Gallery.new("Louvre", "Paris")
+g2 = Gallery.new("MOMA", "NYC")
+
+p1 = Painting.new("Starry Night", 5, g1, nick)
+p2 = Painting.new("Campbell Soup x4", 100, g2, nick)
+p3 = Painting.new("Dragonball Z", 50000, g2, matt)
 
 
 
